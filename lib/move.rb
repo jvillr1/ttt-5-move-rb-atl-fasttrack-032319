2f9c28f81de7ebb1e1288ecd5868e1ec4cdb0e68
Jessica Villarreal <jvillr11@gmail.com>
2019-03-30 21:44:04 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,4 +1,3 @@
-
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -8,49 +7,12 @@ def display_board(board)
 end
 
 def input_to_index(user_input)
-  index = user_input.to_i
-  index -= 1
-  return index
-end
-
-def valid_move?(board, index)
-  def position_taken?(array, ind)
-    if array[ind] == " " || array[ind] == "" || array[ind] == nil
-      return false
-    else
-      return true
-    end
-  end
-
-  def on_board?(num)
-    if num.between?(0, 8) == true
-      return true
-    else
-      return false
-    end
-  end
-
-  if (position_taken?(board, index)) == false && (on_board?(index) == true)
-    return true
-  else
-    return false
-  end
+  new_user_input = user_input.to_i
+  new_user_input -= 1
+  return new_user_input
 end
 
 def move(board, index, character = "X")
   board[index] = character
   return board
 end
-
-def turn(board)
-  puts "Please enter 1-9:"
-  num = gets.chomp
-  index = input_to_index(num)
-  if valid_move?(board, index) == true
-    move(board, index)
-    display_board(board)
-  else
-    turn(board)
-  end
-end
- 
