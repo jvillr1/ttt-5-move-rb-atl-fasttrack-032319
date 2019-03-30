fee59ea740140f1919df1605cce400e31c68c08a
Jessica Villarreal <jvillr11@gmail.com>
2019-03-30 21:39:14 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,21 +1,2 @@
 
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-# code your input_to_index and move method here!
-def input_to_index(choice)
-  choice.to_i - 1
-end
-
-def move(board, index, piece="X")
-  puts "Player wants to move to #{index}"
-  board[index] = piece
-end
-
-
  
