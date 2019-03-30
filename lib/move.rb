8984808977e49c31602eddd1effc326e94e0a0c5
Jessica Villarreal <jvillr11@gmail.com>
2019-03-30 21:40:34 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,4 +1,4 @@
-#displays a tic tac toe board, with board spaces passed as an array
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -7,14 +7,12 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-#coverts a user's place on the board to the index integer
 def input_to_index(user_input)
   index = user_input.to_i
   index -= 1
   return index
 end
 
-#checks the number to see if 1) the position is already taken and 2)it is an actual number
 def valid_move?(board, index)
   def position_taken?(array, ind)
     if array[ind] == " " || array[ind] == "" || array[ind] == nil
@@ -39,14 +37,12 @@ def valid_move?(board, index)
   end
 end
 
-#Takes a users num and the board, and places the X or O char in that position
 def move(board, index, character = "X")
   board[index] = character
   return board
 end
 
-#Asks user for a number, check if it is valid, and if it's not, recursively continues to ask for a number
-def turn (board)
+def turn(board)
   puts "Please enter 1-9:"
   num = gets.chomp
   index = input_to_index(num)
